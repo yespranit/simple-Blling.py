b19ff04c263d3f9c06956220720271737a4a33d4
Pranit Tambade <tambadepranit@gmail.com>
2025-12-01 21:28:30 +0530

    Add files via upload

---
+++ bill.py
@@ -0,0 +1,14 @@
+total = 0
+
+while True:
+    userInput = input("Enter the price (press x to quit): ")
+
+    if userInput.lower() != 'x':    # handles X or x
+        try:
+            total += int(userInput)
+            print(f"Order total so far: {total}")
+        except ValueError:
+            print("Please enter a valid number!")
+    else:
+        print(f"Your final bill is {total}. Thanks for shopping with us!")
+        break
