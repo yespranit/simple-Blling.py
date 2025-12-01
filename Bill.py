6b95b1028630053f14c487dfd964af2ab5f4a03b
Pranit Tambade <tambadepranit@gmail.com>
2025-12-01 21:30:00 +0530

    Update and rename bill.py to Bill.py

--- Bill.py
+++ Bill.py
@@ -5,10 +5,11 @@ while True:
 
     if userInput.lower() != 'x':    # handles X or x
         try:
-            total += int(userInput)
+            total += int(userInput) 
             print(f"Order total so far: {total}")
         except ValueError:
             print("Please enter a valid number!")
     else:
         print(f"Your final bill is {total}. Thanks for shopping with us!")
         break
+
